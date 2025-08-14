7af9788d01ff096f15d120075d9863b89fbaf0ba
XiaoMeng <2556242161@qq.com>
2025-08-14 14:59:58 +0800

    初体验TS

---
+++ WebZiXue/TS/2508-study-ts/dist/01_hello.js
@@ -0,0 +1,9 @@
+function greet(name) {
+    const person = {
+        name: "张三",
+        age: 18,
+    };
+    console.log(person.info.address);
+}
+greet("123");
+export {};
---
+++ WebZiXue/TS/2508-study-ts/src/01_hello.ts
@@ -0,0 +1,8 @@
+function greet(name:string){
+    const person = {
+        name:"张三",
+        age:18,
+    }
+    console.log(person.info.address);
+}
+greet("123");
